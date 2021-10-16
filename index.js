d008c4cecc30c5e3c0596ff9ae34abd36906bb2a
Jorch <jorch.rl@gmail.com>
2021-10-16 18:25:23 -0500

    Finish 3.1 and 3.2

--- index.js
+++ index.js
@@ -23,6 +23,15 @@ app.get("/api/persons", (request, response) => {
     response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+    const currentDate = new Date();
+    response.send(`
+    <div>
+        <h3>Phonebook has info for ${persons.length} people</h3>
+        <p>${currentDate}</p>
+    </div>`);
+});
+
 app.listen(PORT, () => {
     console.log(`Server initiated at http://localhost:${PORT}`);
 });
