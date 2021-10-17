914a0039b1a8e0e3ca67921223533935bf7d234f
Jorch <jorch.rl@gmail.com>
2021-10-16 19:00:08 -0500

    Finish excercise 3.4

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const { response } = require("express");
 const express = require("express");
 const PORT = 3001;
 const app = express();
 
-const persons = [
+let persons = [
     {
         id: 1,
         name: "Pelos el Gato",
@@ -14,6 +15,8 @@ const persons = [
         number: "",
     },
 ];
+
+//// REST Endpoints
 
 app.get("/", (request, response) => {
     response.send("Please use '/api/persons' to interact with this app");
@@ -34,6 +37,18 @@ app.get("/api/persons/:id", (request, response) => {
     response.json(persons.find((p) => p.id === id));
 });
 
+app.delete("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id);
+    const requestedPerson = persons.find((p) => p.id === id);
+    if (requestedPerson === undefined) {
+        return response.status(400).send(`Bad Request: There is no person with id ${id}`);
+    }
+
+    persons = persons.filter((p) => p.id !== id);
+
+    response.status(200).end();
+});
+
 app.get("/info", (request, response) => {
     const currentDate = new Date();
     response.send(`
@@ -42,6 +57,8 @@ app.get("/info", (request, response) => {
         <p>${currentDate}</p>
     </div>`);
 });
+
+/////
 
 app.listen(PORT, () => {
     console.log(`Server initiated at http://localhost:${PORT}`);
